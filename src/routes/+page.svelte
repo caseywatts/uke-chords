d26cd3bd0ed51d30a4f306bc0dc8c0d075627d89
Casey Watts <casey.s.watts@gmail.com>
2023-01-22 15:11:27 -0500

    add in chord wrapper component; extract chords to a json file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,30 @@
-<h1>Uke Chords</h1>
-<div class="grid grid-cols-6 text-center justify-items-center">
-  <span class="bg-slate-200 m-2 p-4 w-16 text-center">C</span>
-  <span class="bg-slate-200 m-2 p-4 w-16 text-center">G</span>
-  <span class="bg-slate-200 m-2 p-4 w-16 text-center">D</span>
-  <span class="bg-slate-200 m-2 p-4 w-16 text-center">A</span>
-  <span class="bg-slate-200 m-2 p-4 w-16 text-center">E</span>
-  <span class="bg-slate-200 m-2 p-4 w-16 text-center">B</span>
+<script>
+	import UkeChordWrapper from '../uke-chord-wrapper.svelte';
+</script>
 
-  <span class="bg-slate-200 m-2 p-4 w-16 text-center">Cm</span>
-  <span class="bg-slate-200 m-2 p-4 w-16 text-center">Gm</span>
-  <span class="bg-slate-200 m-2 p-4 w-16 text-center">Dm</span>
-  <span class="bg-slate-200 m-2 p-4 w-16 text-center">Am</span>
-  <span class="bg-slate-200 m-2 p-4 w-16 text-center">Em</span>
-  <span class="bg-slate-200 m-2 p-4 w-16 text-center">Bm</span>
+<h1>Uke Chords</h1>
+<div class="grid grid-cols-7 text-center justify-items-center">
+  <UkeChordWrapper name="F"/>
+  <UkeChordWrapper name="C"/>
+  <UkeChordWrapper name="G"/>
+  <UkeChordWrapper name="D"/>
+  <UkeChordWrapper name="A"/>
+  <UkeChordWrapper name="E"/>
+  <UkeChordWrapper name="B"/>
+
+  <UkeChordWrapper name="Fm"/>
+  <UkeChordWrapper name="Cm"/>
+  <UkeChordWrapper name="Gm"/>
+  <UkeChordWrapper name="Dm"/>
+  <UkeChordWrapper name="Am"/>
+  <UkeChordWrapper name="Em"/>
+  <UkeChordWrapper name="Bm"/>
+
+  <UkeChordWrapper name="F7"/>
+  <UkeChordWrapper name="C7"/>
+  <UkeChordWrapper name="G7"/>
+  <UkeChordWrapper name="D7"/>
+  <UkeChordWrapper name="A7"/>
+  <UkeChordWrapper name="E7"/>
+  <UkeChordWrapper name="B7"/>
 </div>
---
+++ src/uke-chord-wrapper.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let name = "";
+  import frets from "./uke-chords.json";
+</script>
+
+<span class="bg-slate-200 m-2 p-4 text-center">
+  <uke-chord img frets={frets[name]} name={name}></uke-chord>
+</span>
