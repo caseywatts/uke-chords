21e2a2e680ed021add3c3cbaff100426ce92f849
Casey Watts <casey.s.watts@gmail.com>
2023-01-22 14:43:33 -0500

    grid layout for chords

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
 <h1>Uke Chords</h1>
-<div>
+<div class="grid grid-cols-6 text-center justify-items-center">
+  <span class="bg-slate-200 m-2 p-4 w-16 text-center">C</span>
+  <span class="bg-slate-200 m-2 p-4 w-16 text-center">G</span>
+  <span class="bg-slate-200 m-2 p-4 w-16 text-center">D</span>
+  <span class="bg-slate-200 m-2 p-4 w-16 text-center">A</span>
+  <span class="bg-slate-200 m-2 p-4 w-16 text-center">E</span>
+  <span class="bg-slate-200 m-2 p-4 w-16 text-center">B</span>
 
+  <span class="bg-slate-200 m-2 p-4 w-16 text-center">Cm</span>
+  <span class="bg-slate-200 m-2 p-4 w-16 text-center">Gm</span>
+  <span class="bg-slate-200 m-2 p-4 w-16 text-center">Dm</span>
+  <span class="bg-slate-200 m-2 p-4 w-16 text-center">Am</span>
+  <span class="bg-slate-200 m-2 p-4 w-16 text-center">Em</span>
+  <span class="bg-slate-200 m-2 p-4 w-16 text-center">Bm</span>
 </div>
-
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
