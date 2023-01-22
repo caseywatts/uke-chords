c31bc59c804ceac3b7267ffd4c0d1f55563d6ffb
Casey Watts <casey.s.watts@gmail.com>
2023-01-22 17:26:23 -0500

    add title if present in URL

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,21 @@
 <script>
 	import UkeChordWrapper from '../uke-chord-wrapper.svelte';
+  import { onMount } from "svelte";
+
+  let title = "";
+  
+  onMount(() => {
+    let searchParams = new URLSearchParams(window.location.search);
+    title = searchParams.getAll("title")[0];
+  });
 </script>
 
 
 
 <h1 class="text-center text-2xl p-3">Uke Chords (Accordion Layout) <span class="pl-4 text-lg">by <a class="text-blue-500 underline" href="https://social.coop/@heycaseywattsup">@heycaseywattsup</a></span></h1>
+{#if title}
+  <h2 class="text-center text-4xl p-3">{title}</h2>
+{/if}
 <div class="grid grid-cols-9 text-center justify-items-center">
   <UkeChordWrapper name="Eb"/>
   <UkeChordWrapper name="Bb"/>
