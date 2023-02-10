184633f22a8c246d4c2f5de0985558f56cdce31a
Casey Watts <casey.s.watts@gmail.com>
2023-02-09 21:54:20 -0500

    supports structure as an argument

--- src/components/+just-the-highlights.svelte
+++ src/components/+just-the-highlights.svelte
@@ -1,35 +1,30 @@
 <script>
   import UkeChordWrapper from '../uke-chord-wrapper.svelte';
   import { createParamsArrayStore } from "../lib/URLSearchParamsStore"
+  import Verse from "./+verse.svelte";
+  import Chorus from "./+chorus.svelte";
+  import Bridge from "./+bridge.svelte";
 
   const chorus = createParamsArrayStore("chorus");
   const verse = createParamsArrayStore("verse");
   const bridge = createParamsArrayStore("bridge");
+  const structure = createParamsArrayStore("structure");
 </script>
 
 <div class="text-center justify-items-center p-4">
-  {#if $chorus.length > 0}
-    <div>
-      <div>Chorus</div>
-      {#each $chorus as chord}
-        <UkeChordWrapper name={chord} highlight="panel" />
-      {/each}
-    </div>
-  {/if}
-  {#if $verse.length > 0}
-    <div>
-      <div>Verse</div>
-      {#each $verse as chord}
-        <UkeChordWrapper name={chord} highlight="panel" />
-      {/each}
-    </div>
-  {/if}
-  {#if $bridge.length > 0}
-  <div>
-    <div>Bridge</div>
-    {#each $bridge as chord}
-      <UkeChordWrapper name={chord} highlight="panel" />
-    {/each}
-  </div>
-  {/if}
+  {#each $structure as part}
+    {#if part == "c"}
+      <Chorus chords={$chorus} />
+    {/if}
+    {#if part == "v"}
+      <Verse chords={$verse} />
+    {/if}
+    {#if part == "b"}
+      <Bridge chords={$bridge} />
+    {/if}
+  {:else}
+    <Verse chords={$verse} />
+    <Chorus chords={$chorus} />
+    <Bridge chords={$bridge} />
+  {/each}
 </div>
---
+++ src/components/+verse.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let chords;
+  import UkeChordWrapper from '../uke-chord-wrapper.svelte';
+</script>
+
+{#if chords.length > 0}
+<div>
+  <div>Verse</div>
+  {#each chords as chord}
+    <UkeChordWrapper name={chord} highlight="panel" />
+  {/each}
+</div>
+{/if}
