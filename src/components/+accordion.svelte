4ac8c6ca1fc3bdc3b09146f768c732f1351be64e
Casey Watts <casey.s.watts@gmail.com>
2023-02-09 19:25:29 -0500

    Beginning of scale-editing tool

--- src/components/+accordion.svelte
+++ src/components/+accordion.svelte
@@ -3,10 +3,51 @@ import chords from "../uke-chords.json";
 const chordNames = Object.keys(chords);
 const chordScaleC = ["C", "Dm", "Em", "F", "G", "Am", "Bm"];
 const chordScaleAm = ["Am", "Bm", "C", "Dm", "Em", "F", "G"];
-const chordScale = chordScaleAm;
 const chordScaleNumbersMajor = ["I", "ii", "iii", "IV", "V", "vi", "vii°"];
 const chordScaleNumbersMinor = ["i", "ii°", "III", "iv", "v", "VI", "VII"];
-const chordScaleNumbers = chordScaleNumbersMinor
+const chordScaleTypesMajor = ["", "", "m", "", "", "m", "m"];
+const chordScaleTypesMinor = ["m", "m", "", "m", "m", "", ""];
+
+
+const scaleName = "F" // (major)
+
+const scaleForScaleName = function (scaleName) {
+  const lettersForScale = function (startingNote) {
+    const allLettersLooping = ["A", "B", "C", "D", "E", "F", "G", "A", "B", "C", "D", "E", "F", "G"]
+    const arrayStartIndex = allLettersLooping.indexOf(startingNote);
+    return allLettersLooping.slice(arrayStartIndex).slice(0,7)
+  }
+
+  const letters = lettersForScale(scaleName);
+  
+  const scale = letters.map((letter, i) => {
+    if (scaleName.includes('m')) { // if minor
+      return letter + chordScaleTypesMinor[i];
+    } else { // major
+      return letter + chordScaleTypesMajor[i];
+    }
+  })
+
+  return scale
+}
+
+
+const getScaleWithNumbers = function(scaleName) {
+  const chordScaleNumbersMajor = ["I", "ii", "iii", "IV", "V", "vi", "vii°"];
+  const chordScaleNumbersMinor = ["i", "ii°", "III", "iv", "v", "VI", "VII"];
+
+  const chordScale = scaleForScaleName(scaleName)
+  let chordScaleNumbers = []
+  if (scaleName.includes('m')) { // if minor
+    chordScaleNumbers = chordScaleNumbersMinor;
+  } else { // major
+    chordScaleNumbers = chordScaleNumbersMajor;
+  }
+  return {chordScale, chordScaleNumbers}
+}
+
+const {chordScale, chordScaleNumbers} = getScaleWithNumbers("F")
+
 </script>
 
 <div class="grid grid-cols-9 text-lg">
