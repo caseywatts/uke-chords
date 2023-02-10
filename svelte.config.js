507f2a30585fff8baa9354156c90f5840efa6234
Casey Watts <casey.s.watts@gmail.com>
2023-02-09 20:17:32 -0500

    for github pages, disable prerendering on all pages and fallback to index.html

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,9 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: 'index.html'
+		})
 	},
 	preprocess: vitePreprocess()
 };
